8e3b38d32842de039503120768399d096c67539a
Stino01 <agostino.carrieri01@gmail.com>
2022-04-07 16:47:48 +0200

    [script] bonus

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-//Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
-
-//Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
-
 //BONUS: Utilizzare gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team: cliccando sul pulsante "add" viene creato un nuovo oggetto, il quale viene inserito nell'array iniziale e viene stampata una nuova card con tutte le informazioni inserite dall'utente.
 
 const team = [
@@ -34,7 +30,7 @@ const team = [
         Nome: 'Barbara Ramos',
         Ruolo: 'Graphic Designer',
         Foto: 'barbara-ramos-graphic-designer.jpg'
-    }
+    },
 ];
 
 function printTeam() {
@@ -52,4 +48,36 @@ function printTeam() {
         </div>`
     }
 }
+printTeam();
+
+// BONUS
+function addMember() {
+    const btn = document.getElementById('addMemberButton');
+    btn.style.cursor = 'Pointer';
+    const nome = document.getElementById('name');
+    const ruolo = document.getElementById('role');
+    const img = document.getElementById('image');
+    
+    btn.addEventListener('click', getValue);
+
+    image = [
+        'new-team-member-01.jpg',
+        'new-team-member-02.jpg',
+        'new-team-member-03.jpg',
+        'new-team-member-04.jpg' 
+    ]
+    console.log(image);
+
+    function getValue(containerCard) {
+        nuovoUtente = {
+            Nome: `${nome.value}`,
+            Ruolo: `${ruolo.value}`,
+            Foto: `${img.value}`
+        }
+        team.push(nuovoUtente);
+
+    }
+}
+
+addMember();
 printTeam();
