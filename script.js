a66c6afa5f87dbcb11299f3a0cc7ad705641a130
Stino01 <agostino.carrieri01@gmail.com>
2022-04-07 15:16:23 +0200

    [script] add object array

--- script.js
+++ script.js
@@ -3,3 +3,36 @@
 //Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
 
 //BONUS: Utilizzare gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team: cliccando sul pulsante "add" viene creato un nuovo oggetto, il quale viene inserito nell'array iniziale e viene stampata una nuova card con tutte le informazioni inserite dall'utente.
+
+const team = [
+    {
+        Nome: 'Wayne Barnett',
+        Ruolo: 'Founder & CEO',
+        Foto: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        Nome: 'Angela Caroll',
+        Ruolo: 'Chief Editor',
+        Foto: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        Nome: 'Walter Gordon',
+        Ruolo: 'Office Manager',
+        Foto: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        Nome: 'Angela Lopez',
+        Ruolo: 'Social Media Manager',
+        Foto: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        Nome: 'Scott Estrada',
+        Ruolo: 'Developer',
+        Foto: 'scott-estrada-developer.jpg'
+    },
+    {
+        Nome: 'Barbara Ramos',
+        Ruolo: 'Graphic Designer',
+        Foto: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
